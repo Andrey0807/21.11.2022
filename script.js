af966749d2f28de041b1f5f53ee0d7aa9906d9c4
Andrey <basmsk@mail.ru>
2022-11-22 16:19:47 +0500

    new_practis3

--- script.js
+++ script.js
@@ -20,18 +20,18 @@ const personalMovieDb = {
     privat: false
 };
 
-// for (let i = 0; i < 2; i++) {
-//     const a = prompt('Один из последних просмотренных фильмов?', ''),
-//           b = prompt('На сколько оцениваете его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцениваете его?', '');
 
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDb.movies[a] = b;
-//         console.log('done');
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
-// }
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDb.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
@@ -48,17 +48,17 @@ function rememberMyFilms() {
     }
 }
 
-//rememberMyFilms();
+rememberMyFilms();
 
 
-// if (personalMovieDb.count < 10) {
-//     console.log("Просмотренно довольно мало фильмов");
-// } else if (personalMovieDb.count >= 10 && personalMovieDb > 30) {
-//     console.log("Вы классический зритель");
-// } else {
-//     (personalMovieDb.count <= 30)
-//     console.log("Вы киноман");
-// };
+if (personalMovieDb.count < 10) {
+    console.log("Просмотренно довольно мало фильмов");
+} else if (personalMovieDb.count >= 10 && personalMovieDb > 30) {
+    console.log("Вы классический зритель");
+} else {
+    (personalMovieDb.count <= 30)
+    console.log("Вы киноман");
+};
 
 function detectPersonalLevel() {
     if (personalMovieDb.count < 10) {
@@ -71,7 +71,7 @@ function detectPersonalLevel() {
     }; 
 }
 
-//etectPersonalLevel();
+detectPersonalLevel();
 
 function showMyDb(hidden) {
     if (!hidden) {
@@ -81,7 +81,7 @@ function showMyDb(hidden) {
 
 showMyDb(personalMovieDb.privat);
 
-//console.log(personalMovieDb);
+console.log(personalMovieDb);
 
 function writeYouGenres() {
     for (let i = 1; i <= 3; i++) {
